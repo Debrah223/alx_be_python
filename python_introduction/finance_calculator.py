09436fdbffc32b9da0c207f03071cd7bd3200adc
Debrah223 <debrahmachuki21@gmail.com>
2024-12-13 16:19:19 +0300

    Added the dollar sign on the savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,10 +6,10 @@ total_monthly_expenses = float(input('Enter your total monthly expenses: '))
 # Calculate the monthly savings by subtracting monthly expenses from the monthly income
 monthly_savings = monthly_income - total_monthly_expenses
 # Print the monthly savings
-print(f'Your monthly savings is [monthly_savings:.2f]') #format to 2 decimal places
+print(f'Your monthly savings is $[monthly_savings:.2f]') #format to 2 decimal places
 # We are going to project Annual savings
 annual_interest_rate = 0.05 #5%
 # We calculate the projected savings after one year, incorporating the interest
 projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * annual_interest_rate)
 # print the projceted savings after including interest
-print(f'Projected savings after one year, with interest, is: [projected_savings].2f') #format to 2 decimal places
+print(f'Projected savings after one year, with interest, is: $[projected_savings].2f') #format to 2 decimal places
