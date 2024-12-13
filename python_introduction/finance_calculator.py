c90b72d7b228adb28ace21c3daf8a10fd237a0b0
Debrah223 <debrahmachuki21@gmail.com>
2024-12-13 16:22:05 +0300

    Add a single quote and removed the double quote

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 # Using arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings
 # Prompt the user for their monthly income
-monthly_income = float(input('“Enter your monthly income: '))
+monthly_income = float(input('Enter your monthly income: '))
 # Ask for their total monthly expenses
 total_monthly_expenses = float(input('Enter your total monthly expenses: '))
 # Calculate the monthly savings by subtracting monthly expenses from the monthly income
