dd978eb64dd8dde8fd70182153e4d1eaf005cbf8
Debrah223 <debrahmachuki21@gmail.com>
2024-12-13 23:31:18 +0300

    Add monthly savings and projected savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Using arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings
 # Prompt the user for their monthly income
 monthly_income = float(input('Enter your monthly income: '))
@@ -6,11 +7,11 @@ monthly_expenses = float(input('Enter your total monthly expenses: '))
 # Calculate the monthly savings by subtracting monthly expenses from the monthly income
 monthly_savings = monthly_income - monthly_expenses
 # Print the monthly savings
-print(f'Your monthly savings are: $[monthly_savings:.2f]') #format to 2 decimal places
+print(f'Your monthly savings are: $',monthly_savings) #format to 2 decimal places
 # We are going to project Annual savings
 annual_interest_rate = 0.05 #5%
 # We calculate the projected savings after one year, incorporating the interest
 annual_savings = monthly_savings * 12
 projected_savings = annual_savings + (annual_savings * 0.05)
 # print the projected savings after including interest
-print(f'Projected savings after one year, with interest, is: $[projected_savings:.2f]') #format to 2 decimal places
+print(f'Projected savings after one year, with interest, is: $',projected_savings) #format to 2 decimal places
