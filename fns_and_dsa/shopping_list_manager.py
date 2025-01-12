4af077024fe6900b9385a1c5e9e65703bf9d2b29
Debrah223 <debrahmachuki21@gmail.com>
2025-01-12 15:00:16 +0300

    Added the input on add item

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -8,7 +8,7 @@ def display_menu():
     print("4. Exit")
 
     def add_item():
-        item = input("Enter item to add: ").strip()
+        item = input("Enter the item to add: ").strip()
         shopping_list.append(item)
         print(f"'{item}' has been added to the list.")
 
