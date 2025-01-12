099b154b9c899c6b2c922514e03575b35c0f6f40
Debrah223 <debrahmachuki21@gmail.com>
2025-01-12 15:42:55 +0300

    Removed OFFSET and added 32

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,11 @@
 # Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-OFFSET = 32
 
 def convert_to_celsius(fahrenheit):
     """Converts Fahrenheit to Celsius."""
-    return (fahrenheit - OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     """Converts Celsius to Fahrenheit."""
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + OFFSET
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
