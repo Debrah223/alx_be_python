e572c8b842c7539ee7ac403df3cbf02779f0db09
Debrah223 <debrahmachuki21@gmail.com>
2025-01-17 14:59:08 +0300

    Added withdraw function to print only once

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,12 +12,11 @@ class BankAccount:
 
     def withdraw(self, amount):
         """Withdraw a specified amount if funds are sufficient."""
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return True
-        else:
+        if amount <=0 or amount > self.__account_balance:
             print("Insufficient funds.")
             return False
+        self.__account_balance -= amount
+        return True
     def display_balance(self):
         """Display the current balance."""
         print(f"Current Balance: ${self.__account_balance:.2f}")
