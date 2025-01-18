eac2d2ffcf6158d83aaa061d1f528698669180d0
Debrah223 <debrahmachuki21@gmail.com>
2025-01-18 15:18:11 +0300

    Added a return statement

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -16,7 +16,7 @@ def safe_divide(numerator, denominator):
 
         #Attempt division
         result = numerator / denominator
-        return f"Result: The result of the division is {result}"
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
