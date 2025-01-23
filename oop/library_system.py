512cafe1ed8043ce052d49f7f49732a4c8afbc87
Debrah223 <debrahmachuki21@gmail.com>
2025-01-23 12:59:05 +0300

    Added return statements in all functions

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,7 +5,7 @@ class Book:
         self.author = author
     def __str__(self):
         """We declare the string representation of the book and return a string describing the book"""
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
     
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -16,7 +16,7 @@ class EBook(Book):
     
     def __str__(self):
         """This is a string representation of the book and we shall return a string describing the ebook"""
-        return f"{super().__str__()} [Ebook, {self.file_size}MB]"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size} MB"
     
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -26,7 +26,7 @@ class PrintBook(Book):
 
     def __str__(self):
         """We declare a string representation of printbook and return a string describing the book"""
-        return f"{super().__str__()} [PrintBook, {self.page_count} pages]"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     
 class Library:
     def __init__(self):
@@ -45,6 +45,6 @@ class Library:
         if not self.books:
             print("The library has no books.")
         else:
-            for idx, book in enumerate(self.books, 1):
-                print(f"{idx}. {book}")
+            for book in self.books:
+                print(book)
     
